886b107f7a72170f73148610de6afd075003208e
Steve Schofield <stevebschofield@gmail.com>
2014-08-26 10:43:45 +0100

    Initial Commit

---
+++ braindu-coredata/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  braindu-coredata
+//
+//  Created by Steven Schofield on 26/08/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ braindu-coredataTests/braindu_coredataTests.m
@@ -0,0 +1,39 @@
+//
+//  braindu_coredataTests.m
+//  braindu-coredataTests
+//
+//  Created by Steven Schofield on 26/08/2014.
+//  Copyright (c) 2014 Double Digital. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface braindu_coredataTests : XCTestCase
+
+@end
+
+@implementation braindu_coredataTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
